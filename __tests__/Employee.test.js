a23f080d4fec961a830cf4766d26f11183ec891a
Zachary Clark <zacclark369@gmail.com>
2022-01-13 00:02:28 -0800

    added base tests for employee

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,22 @@
+const Employee = require('..lib/Employee');
+const { test } = require('picomatch');
+
+test("employee gets a name"), () => {
+    const employee = new Employee({name: 'John', id: "240", email: 'John@example.com', role: 'manager'});
+
+}
+
+test('employee gets an email'), () => {
+    const employee = new Employee({name: 'John', id: "240", email: 'John@example.com', role: 'manager'});
+
+}
+
+test('employee gets an id'), () => {
+    const employee = new Employee({name: 'John', id: "240", email: 'John@example.com', role: 'manager'});
+
+}
+
+test('employee gets an role'), () => {
+    const employee = new Employee({name: 'John', id: "240", email: 'John@example.com', role: 'manager'});
+
+}
