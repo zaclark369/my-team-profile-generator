8eab06f470a01f7c5006cc9b35e1f96b6f96831e
Zachary Clark <zacclark369@gmail.com>
2022-01-13 00:08:18 -0800

    began basic coding of classes

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,20 +3,20 @@ const { test } = require('picomatch');
 
 test("employee gets a name"), () => {
     const employee = new Employee({name: 'John', id: "240", email: 'John@example.com', role: 'manager'});
-
+    expect(employee.getName()).toBe('John');
 }
 
 test('employee gets an email'), () => {
     const employee = new Employee({name: 'John', id: "240", email: 'John@example.com', role: 'manager'});
-
+    expect(employee.getEmail()).toBe('John@example.com');('Bjorn');
 }
 
 test('employee gets an id'), () => {
     const employee = new Employee({name: 'John', id: "240", email: 'John@example.com', role: 'manager'});
-
+    expect(employee.getId()).toBe('240');
 }
 
 test('employee gets an role'), () => {
     const employee = new Employee({name: 'John', id: "240", email: 'John@example.com', role: 'manager'});
-
+    expect(employee.getRole()).toBe('manager');
 }
